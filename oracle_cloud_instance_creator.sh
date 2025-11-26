4700ea5a26a4d4ceed959f0a8935dff0ebccbd76
Rafael Luna <82469173+lunarafael@users.noreply.github.com>
2025-11-25 23:25:19 -0300

    Clean up CLI parameters, remove deprecated options, and enhance user feedback messages

--- oracle_cloud_instance_creator.sh
+++ oracle_cloud_instance_creator.sh
@@ -15,6 +15,8 @@ oci iam compartment list
 if [ $? -ne 0 ]; then
     echo "Connection to Oracle cloud is not working. Check your setup and config again!"
     exit 1
+else
+    echo "Connection to Oracle cloud is working!"
 fi
 
 # ----------------------CUSTOMIZE---------------------------------------------------------------------------------------
@@ -25,26 +27,25 @@ requestInterval=60 # seconds
 # VM params
 cpus=4 # max 4 cores
 ram=24 # max 24gb memory
-bootVolume=50 # disk size in gb
+bootVolume=150 # disk size in gb
 
 profile="DEFAULT"
 
 # ----------------------ENDLESS LOOP TO REQUEST AN ARM INSTANCE---------------------------------------------------------
 
 while true; do
+echo "Requesting new ARM instance with $cpus OCPU(s), $ram GB RAM and $bootVolume GB boot volume..."
 
-    oci compute instance launch --no-retry  \
-    --auth api_key \
-    --profile "$profile" \
+    oci compute instance launch --no-retry \
     --display-name big-arm \
-    --compartment-id "$TENANCY_ID" \
     --image-id "$IMAGE_ID" \
     --subnet-id "$SUBNET_ID" \
     --availability-domain "$AVAILABILITY_DOMAIN" \
     --shape 'VM.Standard.A1.Flex' \
     --shape-config "{'ocpus':$cpus,'memoryInGBs':$ram}" \
     --boot-volume-size-in-gbs "$bootVolume" \
+    --compartment-id "$TENANCY_ID" \
     --ssh-authorized-keys-file "$PATH_TO_PUBLIC_SSH_KEY"
-
+    
     sleep $requestInterval
 done
